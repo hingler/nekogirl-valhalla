bceef80eb792e9f974054e5e4ba7c0290afbf298
jamieboy1337 <jamieboy1337@gmail.com>
2022-02-07 01:01:32 -0800

    update lru test to handle insert update test

--- test/lrumaptest.ts
+++ test/lrumaptest.ts
@@ -52,4 +52,35 @@ describe("LRUMap", function() {
     expect(map.get(3)).to.be.equal(3);
 
   });
+
+  it("Should update double inserts", function() {
+    const map = new LRUMap<number, number>(4);
+    map.insert(0, 0);
+    expect(map.size).to.be.equal(1);
+    map.insert(1, 1);
+    expect(map.size).to.be.equal(2);
+    map.insert(2, 2);
+    expect(map.size).to.be.equal(3);
+    map.insert(3, 3);
+    expect(map.size).to.be.equal(4);
+
+    map.insert(0, 5);
+    map.insert(4, 4);
+
+    expect(map.get(0)).to.be.equal(5);
+    expect(map.get(1)).to.be.null;
+    expect(map.get(2)).to.be.equal(2);
+    expect(map.get(3)).to.be.equal(3);
+    expect(map.get(4)).to.be.equal(4);
+
+    map.insert(2, 2);
+    map.insert(5, 6);
+
+    expect(map.get(0)).to.be.equal(5);
+    expect(map.get(1)).to.be.null;
+    expect(map.get(2)).to.be.equal(2);
+    expect(map.get(3)).to.be.null;
+    expect(map.get(4)).to.be.equal(4);
+    expect(map.get(5)).to.be.equal(6);
+  })
 })
